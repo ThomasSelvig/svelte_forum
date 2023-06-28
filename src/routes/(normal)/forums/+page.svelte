58a2d1996e5e6f647c6f6350ee451ee7f215b3d1
ThomasSelvig <inventoxz@gmail.com>
2023-06-28 19:40:47 +0200

    feat: /forums page with async loading, types, and indirect expand

---
+++ src/routes/(normal)/forums/+page.svelte
@@ -0,0 +1,36 @@
+<style>
+    .category {
+        margin: 1rem;
+    }
+</style>
+
+<script lang="ts">
+    import { pb, type ExpandRecord } from "$lib/pocketbase"
+
+    async function get_forums_by_category(): Promise<ExpandRecord[]> {
+        return pb.collection("forum_categories").getFullList({
+            sort: "-id",
+            expand: "forums(category)"
+        })
+    }
+
+</script>
+
+<h2>Forums</h2>
+
+{#await get_forums_by_category()}
+    <h3>loading...</h3>
+{:then categories} 
+
+    {#each categories as category (category.id)}
+        <section class="category">
+            <h3>{category.name}</h3>
+            {#each category.expand["forums(category)"] as forum}
+                <p><a href={`/forums/${forum.id}`}>{forum.name}</a></p>
+            {/each}
+        </section>
+    {/each}
+
+{:catch error}
+    <h3>error: {error}</h3>
+{/await}
