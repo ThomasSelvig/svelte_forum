7fb7fb9a6ea40938ad13459a402c09787b3fb0a4
ThomasSelvig <inventoxz@gmail.com>
2023-06-28 19:38:41 +0200

    feat: types for expand

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -3,6 +3,15 @@ import { writable } from "svelte/store"
 
 import { PUBLIC_PB_HOST } from "$env/static/public"
 
+import type { Record } from "pocketbase";
+
+
+export interface ExpandRecordSingle extends Omit<Record, "expand"> {
+    expand: {[some_expand:string]: Record}
+}
+export interface ExpandRecord extends Omit<Record, "expand"> {
+    expand: {[some_expand:string]: Record[]}
+}
 
 export const pb = new PocketBase(PUBLIC_PB_HOST)
 export const user = writable(pb.authStore.model, function (set) {  
