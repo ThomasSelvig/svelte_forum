040a3f194fd5374ac19f1337dddf0b1c93784b13
ThomasSelvig <inventoxz@gmail.com>
2023-06-28 02:31:48 +0200

    feat: read host from PUBLIC_ env var

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,11 +1,13 @@
 import PocketBase from "pocketbase"
 import { writable } from "svelte/store"
 
+import { PUBLIC_PB_HOST } from "$env/static/public"
 
-export const pb = new PocketBase("http://127.0.0.1:8090")
+
+export const pb = new PocketBase(PUBLIC_PB_HOST)
 export const user = writable(pb.authStore.model, function (set) {  
     pb.authStore.onChange((token, model) => {  
         set(model)
-        // console.log(`user: ${model?.email}`);
+        // TODO maybe stpre `token` to cookie
     }, true)
 })
