fa750a81f050425151c1b7c216490ee8da3907f4
ThomasSelvig <inventoxz@gmail.com>
2023-07-01 15:44:16 +0200

    fix: don't await preload of image

--- src/routes/(auth)/+layout.ts
+++ src/routes/(auth)/+layout.ts
@@ -4,14 +4,14 @@ import type { LayoutLoad } from './$types';
 function preload(src: string) {
     return new Promise(res => {
         let img = new Image()
-        img.src = src
         img.onload = res
+        img.src = src
     })
 }
 
 export const load = (async () => {
     let bg_img_url = "https://picsum.photos/960/1080"
-    await preload(bg_img_url)
+    preload(bg_img_url)
     return {
         bg_img_url
     };
