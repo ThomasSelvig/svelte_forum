30863d2ab5825a8c052ab9167482f98a5c0c50a9
ThomasSelvig <inventoxz@gmail.com>
2023-07-02 04:10:46 +0200

    fix: TS return type was ...[][]

--- src/routes/(normal)/forums/+page.ts
+++ src/routes/(normal)/forums/+page.ts
@@ -5,7 +5,7 @@ import type { PageLoad } from './$types';
 export const load = (async () => {
     return {
         forums_by_category: pb.collection("forum_categories").getFullList<
-            ForumCategoriesResponse<{ "forums(category)": ForumsResponse[] }>[]
+            ForumCategoriesResponse<{ "forums(category)": ForumsResponse[] }>
         >({
             sort: "-id",
             expand: "forums(category)"
