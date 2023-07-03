709292ef34f57d1693a57e17a42f1fcf2db41ba7
ThomasSelvig <inventoxz@gmail.com>
2023-07-03 04:11:47 +0200

    feat: beginning of /user

---
+++ src/routes/(normal)/user/[user]/+page.svelte
@@ -0,0 +1,21 @@
+<style>
+    p {
+        white-space: pre-wrap;
+    }
+</style>
+
+<script lang="ts">
+    import { pb, user } from "$lib/pocketbase";
+	import { writable } from "svelte/store";
+	import type { PageData } from "./$types";
+
+    export let data: PageData
+
+    let user_data
+    async function main() {
+        user_data = writable(await pb.collection("users_public").getOne(data.req_user_id))
+    }
+    main()
+</script>
+
+<p>user: {JSON.stringify($user_data, null, 4) }</p>
