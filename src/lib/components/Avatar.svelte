9f2e55a3ae1646af9360f2763c44541d00261b8d
ThomasSelvig <inventoxz@gmail.com>
2023-07-08 22:24:10 +0200

    feat: total Post style revamp

--- src/lib/components/Avatar.svelte
+++ src/lib/components/Avatar.svelte
@@ -2,7 +2,6 @@
     .avatar {
         display: flex;
         align-items: center;
-        margin: 1rem;
 
         & > img {
             border-radius: 10px;
@@ -19,11 +18,12 @@
 
     export let size: string = "10rem"
     export let view_user: UsersPublicResponse
+    export let margin: string = "1rem"
 
     let avatar_url = pb.files.getUrl(view_user as unknown as Record, view_user.avatar)
 </script>
 
-<div class="avatar icon" style="width: {size}; height: {size};">
+<div class="avatar icon" style="width: {size}; height: {size}; margin: {margin};">
     {#if view_user.avatar}
         <img src={avatar_url} style="width: 100%;" alt="">
     {:else}
--- src/lib/components/post.svelte
+++ src/lib/components/post.svelte
@@ -1,41 +1,15 @@
-<style lang="scss">
-    .post {
-        padding: .5rem 0;
-        display: flex;
-        justify-content: space-between;
-        overflow-wrap: break-word;
 
-        & > *:nth-child(1) {
-            max-width: 40%;
-        }
-
-        & > span {
-            // flex-basis: 100%;  // set to give all equal columns
-            flex-basis: 20%;
-            &:nth-child(1) {
-                flex-basis: 40%;
-            }
-
-            .rating {
-                display: inline-block;
-                padding: 0 .33rem;
-            }
-
-            .ratings {
-                background-color: $main_background;
-                padding: .5rem;
-                vertical-align: middle;
-            }
-        }
-    }
-</style>
 
 <script lang="ts">
-    import MdiThumbsUpDown from '~icons/mdi/thumbs-up-down'
-    import MdiEye from '~icons/mdi/eye'
+    // import MdiThumbsUpDown from '~icons/mdi/thumbs-up-down'
+    // import MdiEye from '~icons/mdi/eye'
     import MdiComment from '~icons/mdi/comment'
+    import MdiPlus from '~icons/mdi/plus'
+    import MdiMinus from '~icons/mdi/minus'
+    import MdiSubdirectoryArrowRight  from "~icons/mdi/subdirectory-arrow-right"
 	import type { ForumsResponse, PostsPublicResponse, UsersPublicResponse } from "$lib/pocketbase-types";
 	import { calc_time_diff } from '$lib/helpers';
+	import Avatar from './Avatar.svelte';
 
     export let post: PostsPublicResponse<
         unknown,
@@ -45,29 +19,114 @@
 
 </script>
 
-<article class="post">
-    <span><a data-sveltekit-preload-data="hover" href={`/forums/${post.forum}/${post.id}`}>{post.title}</a></span>
-    <span>
-        <span class="ratings">
-            <span class="rating">
-                {post.score}
-                <MdiThumbsUpDown class="icon" />
-            </span>
-            <!-- <span class="rating">
-                60
-                <MdiEye />
-            </span> -->
-            <span class="rating">
-                {post.comments_count}
-                <MdiComment class="icon" />
-            </span>
-        </span>
-    </span>
-    <span>
-        <a href={`/user/${post.expand?.author.id}`}>{post.expand?.author.username}</a>
+<div class="container">
+    <!-- top bar -->
+    <div class="meta">
+        <!-- forum -->
         {#if post.expand && "forum" in post.expand}
-            in <a data-sveltekit-preload-data="hover" href={`/forums/${post.expand?.forum.id}`}>{post.expand?.forum.name}</a>
+        <div class="forum">
+            <span class="icon"><MdiSubdirectoryArrowRight /></span>
+            <a data-sveltekit-preload-data="hover" href={`/forums/${post.expand.forum.id}`}>
+                {post.expand.forum.name}
+            </a>
+        </div>
         {/if}
-    </span>
-    <span>Updated {calc_time_diff(post.updated)}</span>
-</article>
+
+        <!-- author -->
+        {#if post.expand && "author" in post.expand}
+        <div class="author">
+            <a href={`/user/${post.expand.author.id}`}>
+                <Avatar margin=".5rem .5rem .5rem 0" size="1.5rem" view_user={post.expand.author}/>
+                {post.expand.author.username}
+            </a>
+        </div>
+        {/if}
+
+        <!-- comments -->
+        <div class="comments">
+            {post.comments_count}
+            <MdiComment class="icon" />
+        </div>
+
+        <!-- time since last update -->
+        <span>{calc_time_diff(post.updated)}</span>
+    </div>
+
+    <div class="post">
+        <!-- left bar -->
+        <div class="controls">
+            <button class="icon text"><MdiPlus /></button>
+            <pre>{post.score}</pre>
+            <button class="icon text"><MdiMinus /></button>
+        </div>
+
+        <!-- post content -->
+        <div class="content">
+            <h2>{post.title}</h2>
+        </div>
+    </div>
+</div>
+
+<style lang="scss">
+    .meta {
+        display: flex;
+        align-items: center;
+        gap: 2rem;
+        margin-left: 10%;
+
+        .author > a {
+            display: flex;
+            align-items: center;
+        }
+    }
+
+    .post {
+        display: grid;
+        align-items: center;
+        grid-template-columns: 1fr 9fr;
+    }
+
+    .controls {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .content h2 {
+        margin: 0;
+    }
+
+    .container {
+        margin: 2rem 0;
+    }
+    // .post {
+    //     padding: .5rem 0;
+    //     display: flex;
+    //     justify-content: space-between;
+    //     overflow-wrap: break-word;
+
+    //     & > *:nth-child(1) {
+    //         max-width: 40%;
+    //     }
+
+    //     & > span {
+    //         // flex-basis: 100%;  // set to give all equal columns
+    //         flex-basis: 20%;
+    //         &:nth-child(1) {
+    //             flex-basis: 40%;
+    //         }
+
+    //         .rating {
+    //             display: inline-block;
+    //             padding: 0 .33rem;
+    //         }
+
+    //         .ratings {
+    //             background-color: $main_background;
+    //             padding: .5rem;
+    //             vertical-align: middle;
+    //         }
+    //     }
+    // }
+</style>
