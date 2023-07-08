abf4719c776157114c51293403b8bcccd1be7b62
ThomasSelvig <inventoxz@gmail.com>
2023-07-08 17:18:32 +0200

    feat: basic pagination buttons

---
+++ src/lib/components/PaginatedList.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import type { ListResult } from "pocketbase";
+
+    export let list: ListResult<any>
+</script>
+
+<!-- 
+    This component doesn't show the items of the list, only the
+    buttons for pagination based on the ListResult. I would've
+    had it like this:
+
+// in some +page.svelte:
+
+<PaginatedList let:item list={posts}>
+    <Post post={item} />
+</PaginatedList>
+
+
+// in PaginatedList.svelte:
+
+{#each list.items as item}
+    <slot {item} />
+{/each}
+
+    if it weren't for the fact that you can't pass types to components,
+    leading to {item} being passed as "any".
+ -->
+
+<div class="meta">
+    Showing {list.items.length} of {list.totalItems}
+</div>
+
+<div class="page_buttons">
+    {#each {length: list.totalItems / list.perPage} as _, page_i}
+        <button class="icon">{page_i}</button>
+    {/each}
+</div>
--- src/routes/(normal)/user/[user]/posts/+page.svelte
+++ src/routes/(normal)/user/[user]/posts/+page.svelte
@@ -6,18 +6,18 @@
 	import { pb } from '$lib/pocketbase';
 	import Loading from '$lib/components/Loading.svelte';
 	import Post from '$lib/components/Post.svelte';
+	import PaginatedList from '$lib/components/PaginatedList.svelte';
     
     export let data: PageData;
 
-    let posts = writable<Promise<ListResult<
-        PostsPublicResponse<unknown, {
-            forum: ForumsResponse,
-            author: UsersPublicResponse
-        }>
-    >>>()
+    type PostsList = ListResult<PostsPublicResponse<unknown, {
+        forum: ForumsResponse,
+        author: UsersPublicResponse
+    }>>
+    let posts = writable<Promise<PostsList>>()
 
     function load_posts(page: number) {
-        $posts = pb.collection("posts_public").getList(page, 20, {
+        $posts = pb.collection("posts_public").getList(page, 2, {
             filter: `author="${data.req_user_id}"`,
             sort: "-created",
             expand: "forum,author"
@@ -28,7 +28,10 @@
 
 <h2>Posts {#await $posts}<Loading />{/await}</h2>
 {#await $posts then posts}
+
     {#each posts.items as post}
         <Post {post} />
     {/each}
+    <PaginatedList list={posts} />
+
 {/await}
